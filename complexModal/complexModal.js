56b82f596ac17033ae56561528006de78409e26c
Sivakkumar <kannantecht@gmail.com>
2023-12-14 20:09:38 +0530

    first commit

---
+++ complexModal/complexModal.js
@@ -0,0 +1,19 @@
+const OpenModelBtn = document.getElementById("open-button");
+const modal = document.getElementById("basic-modal");
+const closeModalBtn = document.getElementById("close-button");
+
+OpenModelBtn.addEventListener("click", openModal);
+closeModalBtn.addEventListener("click", closeModal);
+window.addEventListener("click", clickedOutsideModal);
+
+function openModal(e) {
+  modal.style.display = "flex";
+}
+function closeModal(e) {
+  modal.style.display = "none";
+}
+function clickedOutsideModal(e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+}
